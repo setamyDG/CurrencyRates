492d52a80258987189015ebb6e25504865a67d2f
setamyDG <setamy.dg@gmail.com>
2020-03-08 18:53:21 +0100

    add more currencies

--- components/PickerComponent/PickerComponent.js
+++ components/PickerComponent/PickerComponent.js
@@ -3,7 +3,32 @@ import {View, Picker, StyleSheet} from 'react-native';
 
 class PickerComponent extends React.Component {
   state = {
-    currencies: ['USD', 'AUD', 'SGD', 'PHP', 'EUR', 'PLN', 'GBP'],
+    currencies: [
+      'USD',
+      'AUD',
+      'SGD',
+      'PHP',
+      'EUR',
+      'PLN',
+      'GBP',
+      'CAD',
+      'MXN',
+      'JPY',
+      'HUF',
+      'DKK',
+      'ISK',
+      'RUB',
+      'ZAR',
+      'MYR',
+      'KRW',
+      'TRY',
+      'NOK',
+      'NZD',
+      'CNY',
+      'THB',
+      'CHF',
+      'BGN',
+    ],
   };
 
   render() {
